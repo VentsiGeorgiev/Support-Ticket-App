a2e24c08dd8705508faa8173421029bf1bc430bc
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-08-21 16:12:10 +0200

    Error and Exception Handling

--- server/server.js
+++ server/server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const PORT = process.env.PORT
+const { errorHandler } = require('./middleware/errorMiddleware');
+const PORT = process.env.PORT;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
-  res.json({ message: 'My App' })
-})
+  res.json({ message: 'My App' });
+});
 
 // Routes
-app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
