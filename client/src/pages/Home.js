57f5465baa9d1f923065d7428007c9b333b1fdf9
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-08-27 13:37:05 +0200

    Add Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,23 @@
+import { Link } from 'react-router-dom';
+
 function Home() {
-    return <h1>Home</h1>;
+    return (
+        <>
+            <section>
+                <h1>What do you need help with?</h1>
+                <p>Please choose from an option below</p>
+            </section>
+            <section>
+                <div>
+                    <Link to='/new-ticket'>Create New Ticket</Link>
+                </div>
+                <div>
+                    <Link to='/tickets'>My tickets</Link>
+                </div>
+            </section>
+
+        </>
+    );
 }
 
 export default Home;
