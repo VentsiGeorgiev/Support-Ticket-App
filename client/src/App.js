870dbd8af5fef4bef3263e1b796deadd8e60050f
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-08-27 13:25:58 +0200

    Add Register form

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -6,14 +8,17 @@ import Register from './pages/Register';
 
 function App() {
     return (
-        <Router>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </Router>
+        <>
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </Router>
+            <ToastContainer />
+        </>
     );
 }
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,91 @@
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+
 function Register() {
-    return <h1>Register</h1>;
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        repassword: '',
+    });
+
+    const { name, email, password, repass } = formData;
+
+    const onChange = (e) => {
+        setFormData((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value
+        }));
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+
+        if (password !== repass) {
+            toast.error('Passwords do not match!');
+        }
+    };
+
+    return (
+        <>
+            <section>
+                <h1>Register</h1>
+                <p>Please create an account</p>
+            </section>
+            <section>
+                <form onSubmit={onSubmit}>
+                    <div>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            value={name}
+                            onChange={onChange}
+                            placeholder="Enter your name"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            id="email"
+                            name="email"
+                            value={email}
+                            onChange={onChange}
+                            placeholder="Enter your password"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            value={password}
+                            onChange={onChange}
+                            placeholder="Enter password"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            value={repass}
+                            onChange={onChange}
+                            placeholder="Confirm password"
+                            required
+                        />
+                    </div>
+
+                    <button>Submit</button>
+
+                </form>
+            </section>
+        </>
+    );
 }
 
 export default Register;
