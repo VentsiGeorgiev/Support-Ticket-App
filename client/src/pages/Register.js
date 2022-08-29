6eb29412aef89b61f79e3e6220dc0d7968a3d4a6
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-08-29 16:51:28 +0200

    Register User

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { useSelector, useDispatch } from 'react-redux';
-import { register } from '../features/auth/authSlice';
+import { register, reset } from '../features/auth/authSlice';
 
 function Register() {
     const [formData, setFormData] = useState({
@@ -14,8 +15,20 @@ function Register() {
     const { name, email, password, repass } = formData;
 
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-    const { user, isLoading, isSuccess, message } = useSelector(state => state.auth);
+    const { user, isLoading, isError, isSuccess, message } = useSelector(state => state.auth);
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
+        if (isSuccess || user) {
+            navigate('/');
+        }
+
+        dispatch(reset());
+    }, [isError, isSuccess, user, message, navigate, dispatch]);
 
     const onChange = (e) => {
         setFormData((prev) => ({
@@ -42,7 +55,7 @@ function Register() {
     return (
         <>
             <section>
-                <h1>Register {user}</h1>
+                <h1>Register</h1>
                 <p>Please create an account</p>
             </section>
             <section>
@@ -83,7 +96,7 @@ function Register() {
                     <div>
                         <input
                             type="password"
-                            id="password"
+                            id="repass"
                             name="repass"
                             value={repass}
                             onChange={onChange}
