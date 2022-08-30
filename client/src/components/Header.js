9509fdfc189ee3ac53adf91d015779c52f851212
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-08-30 08:23:33 +0200

    Logout User

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,25 +1,48 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, reset } from '../features/auth/authSlice';
 
 function Header() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const { user } = useSelector((state) => state.auth);
+
+    const OnLogout = () => {
+        dispatch(logout());
+        dispatch(reset());
+        navigate('/');
+    };
+
     return (
         <header>
             <div className="logo"></div>
             <Link to="/">Customer Support</Link>
             <div className="navigation">
                 <ul>
-                    <li>
-                        <Link to="/login">
-                            <FaSignInAlt />
-                            Login
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/register">
-                            <FaUser />
-                            Register
-                        </Link>
-                    </li>
+                    {user
+                        ? (
+                            <>
+                                <li>
+                                    <button onClick={OnLogout}> <FaSignOutAlt /> Logout</button>
+                                </li>
+                            </>)
+                        : (
+                            <>
+                                <li>
+                                    <Link to="/login">
+                                        <FaSignInAlt />
+                                        Login
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="/register">
+                                        <FaUser />
+                                        Register
+                                    </Link>
+                                </li>
+                            </>)}
+
                 </ul>
             </div>
         </header>
--- client/src/features/auth/authSlice.js
+++ client/src/features/auth/authSlice.js
@@ -9,6 +9,7 @@ const initialState = {
     message: '',
 };
 
+// Register user
 export const register = createAsyncThunk(
     'auth/register',
     async (user, thunkAPI) => {
@@ -22,12 +23,18 @@ export const register = createAsyncThunk(
     }
 );
 
+// Login user
 export const login = createAsyncThunk(
     'auth/login',
     async (user, thunkAPI) => {
         console.log(user);
     }
 );
+
+// Logout user
+export const logout = createAsyncThunk('auth/logout', async () => {
+    await authService.logout();
+});
 
 
 export const authSlice = createSlice({
@@ -55,6 +62,9 @@ export const authSlice = createSlice({
                 state.isLoading = false;
                 state.isError = true;
                 state.message = action.payload;
+                state.user = null;
+            })
+            .addCase(logout.fulfilled, (state) => {
                 state.user = null;
             });
     }
