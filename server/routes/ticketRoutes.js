6c2da975edfecb10fb0073a63ce1df0a5674e528
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-08-31 11:16:52 +0200

    Get single ticket, update and delete ( Server )

--- server/routes/ticketRoutes.js
+++ server/routes/ticketRoutes.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
 
-const { getTickets, createTicket } = require('../controllers/ticketController');
+const { getTickets, getTicket, createTicket, updateTicket, deleteTicket } = require('../controllers/ticketController');
 const { protect } = require('../middleware/auth');
 
-router.route('/').get(protect, getTickets).post(protect, createTicket);
+router.route('/')
+    .get(protect, getTickets)
+    .post(protect, createTicket);
+
+router.route('/:id')
+    .get(protect, getTicket)
+    .delete(protect, deleteTicket)
+    .put(protect, updateTicket);
 
 module.exports = router;
