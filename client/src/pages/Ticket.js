6d0d707d3b67e5a6addd65d7075d145109141fa1
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-09-01 09:29:36 +0200

    Close ticket functionality

--- client/src/pages/Ticket.js
+++ client/src/pages/Ticket.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { getTicket, reset } from '../features/tickets/ticketSlice';
+import { useParams, useNavigate } from 'react-router-dom';
+import { getTicket, reset, closeTicket } from '../features/tickets/ticketSlice';
 import { toast } from 'react-toastify';
 
 
@@ -9,6 +9,7 @@ function Ticket() {
     const { ticket, isLoading, isSuccess, isError, message } = useSelector((state) => state.tickets);
 
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const { ticketId } = useParams();
 
     useEffect(() => {
@@ -27,14 +28,28 @@ function Ticket() {
         return <h3>Something went wrong</h3>;
     }
 
+    const onTicketClose = () => {
+        dispatch(closeTicket(ticketId));
+        toast.success('Ticket Closed');
+        navigate('/tickets');
+    };
+
     return (
         <div>
             <h3>Ticket ID: {ticket._id}</h3>
             <p>{ticket.status}</p>
+            <h3>Product:</h3>
+            <p>{ticket.product}</p>
             <h3>Date Submitetted:</h3>
             <p>{new Date(ticket.createdAt).toLocaleString('en-US')}</p>
             <h3>Description of the issue</h3>
             <p>{ticket.description}</p>
+            <div>
+                {ticket.status !== 'closed' && (
+                    <button onClick={onTicketClose}>Close Ticket</button>
+                )}
+            </div>
+
         </div>
     );
 }
